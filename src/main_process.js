6da6600d657be1896c77f87f22c28cf880ab3570
Andre Torres <andre.correa8@fatec.sp.gov.br>
2019-06-19 10:32:49 -0300

    ipc messaging

--- src/main_process.js
+++ src/main_process.js
@@ -1,29 +1,33 @@
-const { app, BrowserWindow } = require("electron");
-const url = require("url");
-const path = require("path");
-const { ipcMain } = require("electron");
+const {app, BrowserWindow} = require('electron')
+const url = require('url')
+const path = require('path')
+const {ipcMain} = require('electron')
 
-let win;
+let win
 
 function createWindow() {
-  win = new BrowserWindow({ heigth: 600, width: 800 });
-  win.loadUrl(
-    url.format({
-      pathname: path.join(__dirname, "index.html"),
-      protocol: "file:",
+   win = new BrowserWindow({width: 800, height: 600})
+   win.loadURL(url.format ({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
       slashes: true
-    })
-  );
+   }))
 }
 
-ipcMain.on("asynchronous-message", (event, arg) => {
-  console.log(arg);
-  event.sender.send("asynchronous reply", "async pong");
-});
+// Event handler for asynchronous incoming messages
+ipcMain.on('asynchronous-message', (event, arg) => {
+   console.log(arg)
 
-ipcMain.on("synchronous-message", (event, arg) => {
-  console.log(arg);
-  event.returnValue = "sync pong";
-});
+   // Event emitter for sending asynchronous messages
+   event.sender.send('asynchronous-reply', 'main_process -> async pong')
+})
 
-app.on('ready', createWindow());
+// Event handler for synchronous incoming messages
+ipcMain.on('synchronous-message', (event, arg) => {
+   console.log(arg) 
+
+   // Synchronous event emmision
+   event.returnValue = 'sync pong'
+})
+
+app.on('ready', createWindow)
